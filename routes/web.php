37fd31ffbb2c2779731df54f06bd6e5bccc7f7c5
ridhoajibx <ridhoajibx@gmail.com>
2020-06-15 15:07:10 +0700

    cara menampilkan post dg model binding

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController');
 
+Route::get('posts/{post:slug}', 'PostController@show');
+
 Route::view('/contact', 'contact');
 Route::view('/about', 'about');
 Route::view('/login', 'login');
