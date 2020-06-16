e53eadca3665d2027651859130334cc452853304
ridhoajibx <ridhoajibx@gmail.com>
2020-06-16 21:00:23 +0700

    change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
     <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
-    <title>{{ $title ?? 'Laravel' }}</title>
+    <title>{{ $title ?? 'Laravel-sev' }}</title>
 </head>
 <body>
     @include('layouts.navigation')
