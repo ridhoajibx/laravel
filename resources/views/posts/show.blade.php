0a3903c323c08b94e3f755bd50714f5536745a8a
ridhoajibx <ridhoajibx@gmail.com>
2020-06-17 02:52:10 +0700

    many to many pada post_tag

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,12 @@
         <h3>{{ $post->title }}</h3>
         <div class="text-secondary">
             <small>
-                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> &middot; {{ $post->created_at->format('d F, Y') }}
+                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> 
+                &middot; {{ $post->created_at->format('d F, Y') }}
+                &middot;
+                @foreach ($post->tags as $tag)
+                    <a href="#">{{ $tag->name }}</a>
+                @endforeach
             </small>
         </div>
         <hr>
