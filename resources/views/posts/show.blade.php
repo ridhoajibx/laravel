ba2c2e158cdfd1dc0f34fca7b26ab53ffc3f0f31
ridhoajibx <ridhoajibx@gmail.com>
2020-06-18 10:26:59 +0700

    fix auth dalam show blade

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,33 +21,34 @@
             <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#exampleModal">
                 Delete
             </button>
-        @endauth
 
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Apa anda yakin menghapusnya?</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div>
-                        <p>{{ $post->title }}</p>
-                        <small>Published : {{ $post->created_at->format('d F, Y') }}</small>
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Apa anda yakin menghapusnya?</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
-                    <div class="d-flex justify-content-end">
-                        <form action="/posts/{{ $post->slug }}/delete" method="post">
-                            @csrf
-                            @method("delete")
-                            <button type="submit" class="btn btn-sm btn-danger">Iya</button>
-                            <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Tidak</button>
-                        </form>
+                    <div class="modal-body">
+                        <div>
+                            <p>{{ $post->title }}</p>
+                            <small>Published : {{ $post->created_at->format('d F, Y') }}</small>
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <form action="/posts/{{ $post->slug }}/delete" method="post">
+                                @csrf
+                                @method("delete")
+                                <button type="submit" class="btn btn-sm btn-danger">Iya</button>
+                                <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Tidak</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        @endauth
     </div>
 @endsection
